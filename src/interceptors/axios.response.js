afa600ee27a0630b0f831bd89c3a4e427757994a
dubeyJi <ashd1014@hotmail.com>
2024-11-10 02:42:51 +0530

    added error interceptor

--- src/interceptors/axios.response.js
+++ src/interceptors/axios.response.js
@@ -15,10 +15,28 @@ import axios from "axios";
 // );
 
 export const axiosResponseInterceptor = (response) => {
-  console.log(response, "interceptor response");
   if (response.status === 401) {
     localStorage.clear();
-    window.location.href = "/login";
+    window.location.href = "/";
   }
   return response;
+};
+
+export const axiosErrorInterceptor = (error) => {
+  if (error.response) {
+    // Server responded with a status other than 2xx
+    if (error.response.status === 401) {
+      localStorage.clear();
+      window.location.href = "/";
+    }
+    console.log("Error response data:", error.response.data);
+  } else if (error.request) {
+    // No response was received
+    console.error("No response received", error.request);
+  } else {
+    // Something happened in setting up the request
+    console.error("Error setting up request", error.message);
+  }
+
+  return Promise.reject(error); // Reject the promise to propagate the error
 };
